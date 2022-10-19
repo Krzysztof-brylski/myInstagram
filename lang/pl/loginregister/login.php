6f284e01eec39c6ca6809e0b04c4e111fcc8897a
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2022-10-19 22:15:34 +0200

    dual lang version of login and register page

---
+++ lang/pl/loginregister/login.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'xd'=>'xd',
+    'input'=>[
+        'email'=>'Adres e-mail: ',
+        'password'=>'Hasło',
+        'submit'=>'Zaloguj się',
+    ],
+    'login_via_fb'=>'Zaloguj się przez Facebook',
+    'forgot_passowrd'=>'Nie pamiętasz hasła?',
+    'no_acc'=>'Nie masz konta?',
+    'register'=>'Zarejestruj się'
+];
---
+++ lang/pl/loginregister/register.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'input'=>[
+        'email'=>'Adres e-mail: ',
+        'name_surname'=>'Imie i nazwisko',
+        'password'=>'Hasło',
+        'username'=>'Nazwa użytkownika',
+        'submit'=>'Zarejestruj się',
+    ],
+    'note'=>'Zarejestruj się aby zobaczyć zdjęcia i filmy od twoich przyjaciół.',
+    'login_via_fb'=>'Zaloguj się przez Facebook',
+    'copy_note'=>'Jest to kopia instarama, wykonana w celach edukacyjnych',
+    'has_acc'=>'Masz już konto ?',
+    'login'=>'Zaloguj się'
+];
